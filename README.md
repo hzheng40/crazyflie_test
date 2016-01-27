017fec6b806df3ea84b6c1fd7906e7417a7c1246
Hongrui Zheng <billyzheng@lawn-128-61-67-29.lawn.gatech.edu>
2016-01-27 09:36:05 -0500

    transferred

---
+++ README.md
@@ -0,0 +1,25 @@
+# crazyflie_test
+##Control crazyflie with gesture
+
+##Requirements for only hovering:
+<br/>[crazyflie_ros](https://www.github.com/whoenig/crazyflie_ros)
+<br/>And this repo 
+##Requirements for gesture control:
+<br/>All the requirements for only hovering
+###Drivers and patches:
+<br/>openni 1.5.x (including openni itself, NITE 1.5.x, Patch for primesense repo: [SensorKinect](https://github.com/avin2/SensorKinect))
+###Repos:
+<br/>Modified openni_tracker repo: [mod_openni_tracker](https://github.gatech.edu/hzheng40/mod_openni_tracker)
+<br/>TF data processing repo: [tf_processing](https://github.gatech.edu/hzheng40/learning_tf)
+<br/>clone all the repos including this one and put them in your workspace
+##To run the project:
+<br/>1. catkin_make
+<br/>2. roslaunch openni_lauch openni_launch.launch
+<br/>3. rosrun mod_openni_tracker mod_openni_tracker
+<br/>4. rosrun learning_tf tf_test
+<br/>5. roslaunch crazyflie_driver crazyflie_server.launch
+<br/>6. roslaunch crazyflie_driver crazyflie_add.launch uri:=//0/80/250k (double check the uri for your crazyflie but it should be this one if you're only running one drone)
+###IMPORTANT: Before running the main program flying the drone, make sure a user is in frame and do the psi pose to start calibration. Make sure the tracker started tracking and tf_test is publishing and updating data
+<br/>7. rosrun crazyfie_test crazyflie_pressure (for pressure controlled thrust)
+<br/>OR: rosrun crazyflie_test crazyflie_test (for constant thrust)
+
---
+++ src/crazyflie_test.cpp
@@ -0,0 +1,195 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Imu.h>
+//#include <std_msgs/Float32.h>
+
+void publish(double, double, double, double);
+void imuCallback(const sensor_msgs::Imu &_msg);
+//void pressureCallback(const std_msgs::Float32::ConstPtr &float_msg);
+void genericCallback(double, double, double, double);
+void gestureCallback(const geometry_msgs::Quaternion &gesture_msg);
+
+ros::Publisher pub;
+
+double kip = 0;
+double kdp = 10;
+double kpp = 5;
+double kir = 0;
+double kdr = 10;
+double kpr = 6;
+
+
+// double kip = 0;
+// double kdp = 0;
+// double kpp = 5;
+// double kir = 0;
+// double kdr = 0;
+// double kpr = 6;
+
+double count = 1;
+double delay = 0;
+
+// double kppr = 40;
+// double kdpr = 100;
+// double kipr = 0;
+
+int command = 0;
+
+double err_pitchsum, err_rollsum;
+//float prev_pressure, err_pressure_sum;
+bool /*pressureUpdate,*/ imuUpdate;
+bool gestureUpdate = 1;
+//float pressure;
+double linear_x,linear_y,angular_x,angular_y;
+
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "crazyflie_test");
+
+	ros::NodeHandle node;
+
+	pub = node.advertise<geometry_msgs::Twist>("cmd_vel",1);
+	ros::Subscriber imu = node.subscribe("imu", 1, imuCallback);
+	//ros::Subscriber pressure = node.subscribe("pressure", 0, pressureCallback);
+	ros::Subscriber gesture = node.subscribe("gesture",1, gestureCallback);
+
+	ros::Rate loop_rate(50);
+
+	while (ros::ok()) {
+		ros::spinOnce();
+	}
+	return 0;
+}
+
+
+void publish(double pitch, double roll, double yawrate, double thrust) {
+	geometry_msgs::Twist cmd_vel;
+	cmd_vel.linear.x = pitch;
+	cmd_vel.linear.y = roll;
+	cmd_vel.linear.z = thrust;
+	cmd_vel.angular.z = yawrate;
+	pub.publish(cmd_vel);
+	return;
+}
+
+void genericCallback(/*float pressure,*/ double linear_x, double linear_y, double angular_x, double angular_y, int command) {
+	count++;
+	delay += 0.1;
+	double err_pitch = -linear_x;
+	double err_roll = -linear_y;
+	double err_pitchrate = -angular_x;
+	double err_rollrate = -angular_y;
+	err_pitchsum += err_pitch;
+	err_rollsum += err_roll;
+	if (count == 20) {
+		err_pitchsum = 0;
+		err_rollsum = 0;
+		count = 1;
+	}
+	err_rollsum /= count;
+	err_pitchsum /= count;
+	//float err_pressure = pressure - prev_pressure;
+	//err_pressure_sum += err_pressure;
+	//prev_pressure = pressure;
+
+	double pitch = kpp*err_pitch + kip*err_pitchsum + kdp*err_pitchrate;
+	pitch -= 11.7;
+	double roll = kpr*err_roll + kir*err_rollsum + kdr*err_rollrate;
+	roll += 8.5;
+	//double thrust = kppr*pressure + kipr*err_pressure_sum + kdpr*err_pressure;
+	double thrust = 0;
+	if (delay < 10) {
+		thrust = 46000;
+	}else {
+		thrust = 42000;
+	}
+	// if (pressure == NULL || pressure == 0) {
+	// 	thrust =0;
+	// }
+	// if (command == 0 || command == 1) {
+	// 	publish(pitch, roll, 0, thrust);
+	// }
+	if (command == 2) {
+		thrust += 2000;
+	}
+	if (command == 3) {
+		thrust -= 2000;
+	}
+	if (command == 4) {
+		roll -= 1;
+	}
+	if (command == 5) {
+		roll += 1;
+	}
+	if (command == 6) {
+		pitch += 1;
+	}
+	if (command == 7) {
+		pitch -= 1;
+	}
+	publish(pitch, roll, 0, thrust);
+	//ROS_INFO("pitch: %f   roll: %f   thrust: %f   \n", pitch, roll, thrust);
+	ROS_INFO("COMMAND: %d \n", command);
+
+	return;
+}
+// 0/1:hover 2:higher 3:lower 4:roll left 5:roll right 6:pitch forward 7:pitch backwards
+void gestureCallback(const geometry_msgs::Quaternion &gesture_msg) {
+	double gesture_x = gesture_msg.x;
+	double gesture_y = gesture_msg.y;
+	double gesture_z = gesture_msg.z;
+	double gesture_hold = gesture_msg.w;
+
+	//if left hand is up, hold the drone hover no matter what right hand is doing
+	if (gesture_hold >= 0.1) {
+		command = 1;
+	} else {
+		if (gesture_x < 0) {
+			command = 4;
+		} else if (gesture_x > 0.6) {
+			command = 5;
+		} else if (gesture_y > 0.5) {
+			command = 2;
+		} else if (gesture_y < -0.2) {
+			command = 3;
+		} else if (gesture_z < -0.3) {
+			command = 6;
+		} else if (gesture_z > 0.3) {
+			command = 7;
+		}
+	}
+
+	gestureUpdate = true;
+	if (/*pressureUpdate && */imuUpdate && gestureUpdate) {
+		genericCallback(/*pressure,*/ linear_x, linear_y, angular_x, angular_y, command);
+	}
+}
+
+// void pressureCallback(const std_msgs::Float32::ConstPtr &float_msg) {
+// 	pressure = float_msg->data;
+// 	pressureUpdate = true;
+// 	if (pressureUpdate && imuUpdate && gestureUpdate) {
+// 		genericCallback(pressure, linear_x, linear_y, angular_x, angular_y, command);
+// 	}
+// 	//ROS_INFO("Pressure: %f \n", pressure);
+// 	return;
+// }
+void imuCallback(const sensor_msgs::Imu &_msg) {
+
+	angular_x = _msg.angular_velocity.x;
+	angular_y = _msg.angular_velocity.y;
+	linear_x = _msg.linear_acceleration.x;
+	linear_y = _msg.linear_acceleration.y;
+	
+	//linear_x can be regarded as pitch, linear_y can be regarded as roll
+	//angular_x can be regarded as pitch rate, angular_y can be regarded
+	//as roll rate
+	//ignore yaw at the moment since the quadcopter handles it pretty good
+
+	imuUpdate = true;
+	if (imuUpdate && /*pressureUpdate &&*/ gestureUpdate) {
+		genericCallback(/*pressure, */linear_x, linear_y, angular_x, angular_y, command);
+	}
+	//publish(0, 0, 0, 38000);
+	//ROS_INFO("pitch: %f      roll: %f\n", pitch, roll);
+	return;
+}
